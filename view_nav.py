81dc196ed4fe9a5548bf3dbc7fd13fbfddb13bd4
JADHERMONTALVAO <jadhermontalvao@outlook.com>
2026-04-27 20:50:41 -0300

    Delete view_nav.py

--- view_nav.py
+++
@@ -1,5 +0,0 @@
-﻿from pathlib import Path
-text=Path('index.html').read_text(encoding='utf-8', errors='replace')
-start=text.find('via-navbar w-nav')
-print('start', start)
-print(text[start-80:start+1500])
