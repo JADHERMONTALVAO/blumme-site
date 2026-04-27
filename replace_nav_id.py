dfeee5e5dcafc631f34542749e9eaae3f7a54bdc
JADHERMONTALVAO <jadhermontalvao@outlook.com>
2026-04-27 20:54:36 -0300

    Delete replace_nav_id.py

--- replace_nav_id.py
+++
@@ -1,8 +0,0 @@
-﻿from pathlib import Path
-path = Path('index.html')
-text = path.read_text(encoding='utf-8')
-old = 'nav role= navigation id=via-mobile-menu'
-if old not in text:
-    raise SystemExit('old not found')
-text = text.replace(old, 'nav role=navigation id=via-primary-menu', 1)
-path.write_text(text, encoding='utf-8')
