8c99b2f77554dabd3b0f22fa52a72eecfa4d7253
JADHERMONTALVAO <jadhermontalvao@outlook.com>
2026-04-27 20:55:03 -0300

    Delete replace_aria.ps1

--- replace_aria.ps1
+++
@@ -1,5 +0,0 @@
-﻿ = 'index.html'
- = Get-Content  -Raw
- = [regex]::Escape('aria-controls= via-mobile-menu')
- = [regex]::Replace(, , 'aria-controls=via-mobile-panel', 1)
-Set-Content -Path  -Value  -Encoding utf8
